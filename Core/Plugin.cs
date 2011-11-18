e3be1fd29d3316359ddfca91e792cb6663b95861
Lyubomir Yanchev <lljubo@gmail.com>
2011-11-18 09:01:12 +0200

    Written PluginHandler

---
+++ Core/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace ModernSteward
+{
+    public class Plugin
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Keyword
+        {
+            get;
+            set;
+        }
+
+        public string Command
+        {
+            get;
+            set;
+        }
+
+        Plugin(string aName, string aKeyword, string aCommand)
+        {
+            Name = aName;
+            Keyword = aKeyword;
+            Command = aCommand;
+        }
+
+        public void TriggerPlugin(string aAdditionalCommands)
+        {
+            Console.WriteLine("Command{0}\nAdditionalCommands{1}\n", Command, aAdditionalCommands);
+        }
+    }
+}
---
+++ Core/PluginHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModernSteward
+{
+    public class PluginHandler
+    {
+        List<Plugin> Plugins;
+
+        public bool FindPluginAndTrigger(string aKeyword, string aAdditionalCommands)
+        {
+            bool found = false;
+            foreach(var plugin in Plugins){
+                if (plugin.Keyword == aKeyword)
+                {
+                    TriggerEvent(plugin, aAdditionalCommands);
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        private void TriggerEvent(Plugin aPlugin, string aAdditionalCommands)
+        {
+            aPlugin.TriggerPlugin(aAdditionalCommands);
+        }
+    }
+}
