4f6a15f7055926b6eafacdd77f98f2f2066d85f4
Lyubomir Yanchev <lljubo@gmail.com>
2011-12-23 00:05:30 +0200

    SpeechGrammarBuilder made... couple of bugs in the UI to fix...

--- SpeechGrammarBuilderGUI/SpeechGrammarBuilder.Designer.cs
+++ SpeechGrammarBuilderGUI/SpeechGrammarBuilder.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.treeViewCommands = new System.Windows.Forms.TreeView();
+            this.buttonWriteInConsoleTheTree = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeViewCommands
@@ -38,11 +39,22 @@
             this.treeViewCommands.Size = new System.Drawing.Size(259, 237);
             this.treeViewCommands.TabIndex = 0;
             // 
+            // buttonWriteInConsoleTheTree
+            // 
+            this.buttonWriteInConsoleTheTree.Location = new System.Drawing.Point(197, 254);
+            this.buttonWriteInConsoleTheTree.Name = "buttonWriteInConsoleTheTree";
+            this.buttonWriteInConsoleTheTree.Size = new System.Drawing.Size(75, 51);
+            this.buttonWriteInConsoleTheTree.TabIndex = 1;
+            this.buttonWriteInConsoleTheTree.Text = "Try writing it in the console!";
+            this.buttonWriteInConsoleTheTree.UseVisualStyleBackColor = true;
+            this.buttonWriteInConsoleTheTree.Click += new System.EventHandler(this.buttonWriteInConsoleTheTree_Click);
+            // 
             // SpeechGrammarBuilderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 308);
+            this.Controls.Add(this.buttonWriteInConsoleTheTree);
             this.Controls.Add(this.treeViewCommands);
             this.Name = "SpeechGrammarBuilderForm";
             this.Text = "SpeechGrammarBuilder";
@@ -53,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeViewCommands;
+        private System.Windows.Forms.Button buttonWriteInConsoleTheTree;
     }
 }
 
--- SpeechGrammarBuilderGUI/SpeechGrammarBuilder.cs
+++ SpeechGrammarBuilderGUI/SpeechGrammarBuilder.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Linq.Expressions;
+using System.Speech.Recognition;
 
 namespace SpeechGrammarBuilderGUI
 {
@@ -22,6 +23,8 @@ namespace SpeechGrammarBuilderGUI
 
             InitializeNodeContextMenu();
 
+            treeViewCommands.LabelEdit = true;
+
             treeViewCommands.ContextMenu = new ContextMenu();
             treeViewCommands.ContextMenu.MenuItems.Add("Add first node", 
                 lambdaEvent += (sender, args) => {
@@ -29,13 +32,31 @@ namespace SpeechGrammarBuilderGUI
                     treeViewCommands.Nodes[treeViewCommands.Nodes.Count - 1].ContextMenu = nodeContextMenu;
                 });
 
-            //treeViewCommands.Nodes.Add("first node");
+            
+
+            //MessageBox.Show(ExportGrammarBuilder().DebugShowPhrases);
+
+
+            treeViewCommands.Nodes.Add("first node");
+            treeViewCommands.Nodes[0].Nodes.Add("1.1");
+            treeViewCommands.Nodes[0].Nodes[0].Nodes.Add("1.1.1");
+            treeViewCommands.Nodes[0].Nodes[0].Nodes.Add("1.1.2");
+            treeViewCommands.Nodes[0].Nodes.Add("1.2");
+            treeViewCommands.Nodes[0].Nodes[1].Nodes.Add("1.2.1");
+            treeViewCommands.Nodes[0].Nodes[1].Nodes.Add("1.2.2");
+            treeViewCommands.Nodes.Add("second node");
+            treeViewCommands.Nodes[1].Nodes.Add("2.1");
+            treeViewCommands.Nodes[1].Nodes[0].Nodes.Add("2.1.1");
+            treeViewCommands.Nodes[1].Nodes[0].Nodes.Add("2.1.2");
+
+            treeViewCommands.ExpandAll();
+
         }
 
         private void InitializeNodeContextMenu()
-        {
+        {            
             nodeContextMenu.MenuItems.Add("Add custom command", new EventHandler(ContextMenuAddCustomComamand_OnClick));
-            nodeContextMenu.MenuItems.Add("Add custom wildcard", new EventHandler(ContextMenuAddCustomWildcard_OnClick));
+            nodeContextMenu.MenuItems.Add("Add custom choises", new EventHandler(ContextMenuAddCustomChoises_OnClick));
             nodeContextMenu.MenuItems.Add("Add full wildcard", new EventHandler(ContextMenuAddFullWildCard_OnClick));
             nodeContextMenu.MenuItems.Add("Edit node", new EventHandler(ContextMenuEditNode_OnClick));
             nodeContextMenu.MenuItems.Add("Delede node", new EventHandler(ContextMenuDeleteNode_OnClick));
@@ -43,16 +64,18 @@ namespace SpeechGrammarBuilderGUI
 
         void AddNode(TreeNode aParrentNode, string aName)
         {
+            treeViewCommands.BeginUpdate();
             aParrentNode.Nodes.Add(aName);
             aParrentNode.Nodes[aParrentNode.Nodes.Count - 1].ContextMenu = nodeContextMenu;
+            treeViewCommands.EndUpdate();
         }
 
         void ContextMenuAddCustomComamand_OnClick(object s, EventArgs args)
         {
-            throw new NotImplementedException();
+            AddNode(treeViewCommands.SelectedNode, "CustomCommand");
         }
 
-        void ContextMenuAddCustomWildcard_OnClick(object s, EventArgs args)
+        void ContextMenuAddCustomChoises_OnClick(object s, EventArgs args)
         {
             throw new NotImplementedException();
         }
@@ -70,6 +93,50 @@ namespace SpeechGrammarBuilderGUI
         void ContextMenuDeleteNode_OnClick(object s, EventArgs args)
         {
             throw new NotImplementedException();
+        }
+
+        private GrammarBuilder MakeGrammarBuilderRecursively(TreeNode currentNode)
+        {
+            GrammarBuilder currentGrammar;
+            if (currentNode.Text != "WILDCARD")
+            {
+                currentGrammar = new GrammarBuilder(currentNode.Text);
+            }
+            else
+            {
+                currentGrammar = new GrammarBuilder();
+                currentGrammar.AppendWildcard();
+            }
+            Choices tempGrammar = new Choices();
+
+            for (int i = 0; i < currentNode.Nodes.Count; ++i)
+            {
+                tempGrammar.Add(MakeGrammarBuilderRecursively(currentNode.Nodes[i]));
+            }
+            currentGrammar.Append(tempGrammar);
+            Console.WriteLine(currentNode.Text);
+            return currentGrammar;
+        }
+
+        public GrammarBuilder ExportGrammarBuilder(TreeView treeView)
+        {
+            GrammarBuilder currentGrammar = new GrammarBuilder();
+            Choices wholeGrammar = new Choices();
+            for (int i = 0; i < treeViewCommands.Nodes.Count; ++i)
+            {
+                currentGrammar = MakeGrammarBuilderRecursively(treeViewCommands.Nodes[i]);
+                wholeGrammar.Add(new Choices(currentGrammar));
+            }
+
+            return wholeGrammar.ToGrammarBuilder();
+        }
+
+        private void buttonWriteInConsoleTheTree_Click(object sender, EventArgs e)
+        {
+            GrammarBuilder wholeGrammar = ExportGrammarBuilder(treeViewCommands);
+
+            MessageBox.Show(wholeGrammar.DebugShowPhrases);
+            Console.WriteLine("{0}", wholeGrammar.DebugShowPhrases);
         }
     }
 }
